c6621437fbc1aa7ef0a1057f9aa443b4c8b8a378
Jacques Garré <jcqs.gr@gmail.com>
2020-03-23 21:38:28 +0100

    adding france historical data to tab 2

--- tab2.py
+++ tab2.py
@@ -9,6 +9,7 @@ colors = colors.get()
 data = hs.get_data() 
 
 world_data = data[data['Country'] == 'World']
+france_data = data[data['Country'] == 'France']
 
 def get_content():
   return html.Div([
@@ -22,7 +23,27 @@ def get_content():
           {'x': world_data['Date'], 'y': world_data['New Deaths'], 'type': 'bar', 'name': 'New Deaths'}
         ],
         'layout': {
-          'title': "Evolution of Covid-19 since 31/12/2019",
+          'title': "Evolution of Covid-19 in the World since 31/12/2019",
+          'plot_bgcolor': colors['background'],
+          'bargap': '2',
+          'paper_bgcolor': colors['background'],
+          'font': {
+              'color': colors['text']
+          }
+        }
+      }
+    ),
+    dcc.Graph(
+      id='historical-france-data',
+      figure={
+        'data': [
+          {'x': france_data['Date'], 'y': france_data['Total Cases'], 'type': 'bar', 'name': 'Total Cases'},
+          {'x': france_data['Date'], 'y': france_data['Total Deaths'], 'type': 'bar', 'name': 'Total Deaths'},
+          {'x': france_data['Date'], 'y': france_data['New Cases'], 'type': 'bar', 'name': 'New Cases'},
+          {'x': france_data['Date'], 'y': france_data['New Deaths'], 'type': 'bar', 'name': 'New Deaths'}
+        ],
+        'layout': {
+          'title': "Evolution of Covid-19 in France since 31/12/2019",
           'plot_bgcolor': colors['background'],
           'bargap': '2',
           'paper_bgcolor': colors['background'],
